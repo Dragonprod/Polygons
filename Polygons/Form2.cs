e0adf06de316d585c88fcf80e4c0c28eb1b00b99
Dragonprod <artyom2002-98@mail.ru>
2018-10-23 21:01:29 +0300

    radius change, only delegate

--- Polygons/Form2.cs
+++ Polygons/Form2.cs
@@ -13,19 +13,36 @@ namespace Polygons
     
     public partial class Form2 : Form
     {
+        Shape shape;
         public Form2()
         {
             InitializeComponent();
         }
-       
-        private void Form2_Load(object sender, EventArgs e)
+
+        public void Show1(Shape shape)
         {
-            
+            this.shape = shape;
+            shape.Changed += Shape_Changed;
         }
 
+        private void Shape_Changed(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            shape.Changed -= Shape_Changed;
+        }
         private void trackBar1_ValueChanged(object sender, EventArgs e)
         {
-            
+            shape.RADIUS = (sender as TrackBar).Value;
         }
+
     }
 }
